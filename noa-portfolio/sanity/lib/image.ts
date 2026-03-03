e28af9add548d42bb5f3bebed41e873b090b6f8a
GalaxyNukes <jensdemeyer18@gmail.com>
2026-03-04 00:32:13 +0100

    Update image.ts

--- noa-portfolio/sanity/lib/image.ts
+++ noa-portfolio/sanity/lib/image.ts
@@ -2,8 +2,15 @@ import imageUrlBuilder from "@sanity/image-url";
 import { client } from "../sanity.client";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-const builder = imageUrlBuilder(client);
+const builder = client ? imageUrlBuilder(client) : null;
 
 export function urlFor(source: SanityImageSource) {
+  if (!builder) {
+    return {
+      width: () => ({ height: () => ({ url: () => "" }) }),
+      height: () => ({ url: () => "" }),
+      url: () => "",
+    } as any;
+  }
   return builder.image(source);
 }
